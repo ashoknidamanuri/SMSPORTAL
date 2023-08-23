53974f0933cd78496a0dd9fc030e1d68ef718182
ashoknidamanuri <nidamanuri07@gmail.com>
2023-08-23 11:08:56 +0530

    Listers updated

--- src/main/java/com/qa/smsportal/factory/DriverFactory.java
+++ src/main/java/com/qa/smsportal/factory/DriverFactory.java
@@ -1,11 +1,14 @@
 package com.qa.smsportal.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-
+import org.aspectj.util.FileUtil;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -83,6 +86,21 @@ public class DriverFactory {
 			e.printStackTrace();
 		}
 		return prop;
+	}
+	
+	/**
+	 * take screenshot
+	 */
+	public static String getScreenshot() {
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+		try {
+			FileUtil.copyFile(srcFile, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
 	}
 	
 	
